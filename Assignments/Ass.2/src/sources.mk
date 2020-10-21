06e51b350ae78e1c3ae6b17e60b362aaadfb8638
Mohammed Abdelalim <MohamedLemo14@gmail.com>
2020-10-22 00:09:37 +0200

    Handled a bug when building for host compiler

--- Assignments/Ass.2/src/sources.mk
+++ Assignments/Ass.2/src/sources.mk
@@ -9,23 +9,24 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES = main.c \
-	  memory.c \
-	  startup_msp432p401r_gcc.c \
-	  system_msp432p401r.c \
-	  interrupts_msp432p401r_gcc.c
+ifeq ($(PLATFORM),MSP432)
+	SOURCES = *.c
 
-# Add your include paths to this variable
-INCLUDES = ../include/CMSIS/cmsis_gcc.h \
-	   ../include/CMSIS/core_cm4.h \
-	   ../include/CMSIS/core_cmFunc.h \
-	   ../include/CMSIS/core_cmInstr.h \
-	   ../include/CMSIS/core_cmSimd.h \
-	   ../include/common/memory.h \
-	   ../include/common/platform.h \
-	   ../include/msp432/msp432p401r.h \
-	   ../include/msp432/msp_compatibility.h \
-	   ../include/msp432/system_msp432p401r.h
+	INCLUDES = ../include/CMSIS/cmsis_gcc.h \
+	   	../include/CMSIS/core_cm4.h \
+	   	../include/CMSIS/core_cmFunc.h \
+	   	../include/CMSIS/core_cmInstr.h \
+	   	../include/CMSIS/core_cmSimd.h \
+	   	../include/common/memory.h \
+	   	../include/common/platform.h \
+	   	../include/msp432/msp432p401r.h \
+	   	../include/msp432/msp_compatibility.h \
+	   	../include/msp432/system_msp432p401r.h
 
+else
+	SOURCES = main.c \
+		  memory.c
 
+	INCLUDES = ../include/common/memory.h \
+		../include/common/platform.h
+endif
